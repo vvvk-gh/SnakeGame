33e4db047380122eac95764b27c17a349e0e5768
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-09 20:50:31 +0530

    created man with canvas

--- practice.js
+++ practice.js
@@ -1,20 +1,44 @@
 var canvas = document.getElementById("myCanvas");
+canvas.width = 700;
+canvas.height = 700;
+
 var pen = canvas.getContext('2d');
 
-//sets  the bg-color of the drawings
-pen.fillStyle = "#000fea";
+//creating a rect object
+rect = {
+    x:200,
+    y:200,
+    h:200,
+    w:200
+}
 
-//places the rectangle in (x-axis , y-axis ,width , height)
-pen.fillRect(20,20,200, 200)
+//circle in stroke style
+//left man eye
 
-//places the circle positioned at (x-axis , y-axis ,radius, startAngle , endAngle)
-pen.arc(600,300,50,0,2*Math.PI);
+pen.strokeStyle = "black"
+pen.arc(270 ,140 , 7 ,0 , 2*Math.PI)
+//pen.stroke()
 
-//sets the border color
-pen.strokeStyle = "red";
+//circle in fill mode
+pen.fillStyle = "black";
+pen.arc(315 ,140 , 7 ,0 , 2*Math.PI)
+pen.fill()
 
-//displays the circle
-pen.stroke();
+//rectangle in stroke style
+//man head
+pen.strokeStyle = "black";
+pen.strokeRect(rect.x , rect.y , rect.w , rect.h);
 
-//fills all the objects with the color it is set in fillStyle
-pen.fill();
+//man stomatch
+pen.strokeStyle = "black"
+pen.strokeRect(250 , 100 , 100 , 100)
+
+//rectangle in fill mode
+//left leg 
+pen.fillStyle = "black"
+pen.fillRect(260 , 400 , 10 , 100)
+
+//right leg 
+pen.fillStyle = "black"
+pen.fillRect(340 , 400 , 10 , 100)
+
