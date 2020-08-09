7c0e464b5e8acb452229c98d8a29b7bf465ba0a9
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-10 00:27:14 +0530

    Game loop:  simple bounce back game

--- practice.js
+++ practice.js
@@ -1,44 +1,49 @@
-var canvas = document.getElementById("myCanvas");
-canvas.width = 700;
-canvas.height = 700;
+//implementing Game Loop
+//using 3 methods 
+//init , draw , update :  init runs only once but draw and update has to run constantly
 
-var pen = canvas.getContext('2d');
+//init : intializes all the code 
+function init(){
+canvas = document.getElementById("myCanvas");
+W = canvas.width = 700;
+H = canvas.height = 300;
+pen = canvas.getContext('2d');
 
-//creating a rect object
 rect = {
-    x:200,
-    y:200,
-    h:200,
-    w:200
+    x : 10,
+    y : 10,
+    w : 40,
+    h : 40,
+    speed : 10
 }
 
-//circle in stroke style
-//left man eye
 
-pen.strokeStyle = "black"
-pen.arc(270 ,140 , 7 ,0 , 2*Math.PI)
-//pen.stroke()
+}
 
-//circle in fill mode
-pen.fillStyle = "black";
-pen.arc(315 ,140 , 7 ,0 , 2*Math.PI)
-pen.fill()
+//draw
+function draw(){
+    pen.clearRect(0,0, W, H)
+    pen.fillStyle ="red"
+    pen.fillRect(rect.x , rect.y ,rect.w , rect.h)
+}
 
-//rectangle in stroke style
-//man head
-pen.strokeStyle = "black";
-pen.strokeRect(rect.x , rect.y , rect.w , rect.h);
+//update
+function update(){
+    
+    if(rect.x > W-40 || rect.x < 0){
+        console.log("reversed")
+        rect.speed *= -1;
+    }
+    rect.x += rect.speed
+}
 
-//man stomatch
-pen.strokeStyle = "black"
-pen.strokeRect(250 , 100 , 100 , 100)
+//gameLoop
+function gameLoop (){
+    draw();
+    update();
+}
 
-//rectangle in fill mode
-//left leg 
-pen.fillStyle = "black"
-pen.fillRect(260 , 400 , 10 , 100)
-
-//right leg 
-pen.fillStyle = "black"
-pen.fillRect(340 , 400 , 10 , 100)
-
+//calling init
+init();
+//Calling Game Loop
+setInterval(gameLoop , 100)
