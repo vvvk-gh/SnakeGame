eef5d446b8b8cfe0d324aa6c0a868efc0772ef78
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-11 16:50:31 +0530

    snake movement

--- snake.js
+++ snake.js
@@ -1,9 +1,9 @@
 //implementing Game Loop
 function init(){
     var canvas = document.getElementById('myCanvas')
-    W = H = canvas.width = canvas.height = 1000;
+    W = H = canvas.width = canvas.height = 700;
     pen = canvas.getContext('2d')
-    cs = 50; //cell size
+    cs = 40; //cell size
     
     //Creating snake object
     snake = {
@@ -23,6 +23,13 @@ function init(){
                     pen.fillStyle = this.color
                     pen.fillRect(this.cells[i].x *cs , this.cells[i].y *cs , cs-3 , cs-3);
                 }
+        },
+        updateSnake : function (){
+            //for movement
+            this.cells.pop() //removing last cell
+            var headX = this.cells[0].x; // getting head cell x -axis
+            var headY = this.cells[0].y; // getting head cell y -axis
+            this.cells.unshift({x:headX+1 , y :headY}) // adding an cell with postined at x+1 before head 
         }
 
     }
@@ -31,12 +38,21 @@ function init(){
 }
 
 function draw(){
+   //if snake reaches boundaries alert and stop interval
+   if(snake.cells[0].x * cs>= W || snake.cells[0].y >= H || snake.cells[0].x == 0 ){
+       alert("Game Over")
+       clearInterval(f);
+   }
+   //else do the below
    //drawing the snake on screen
-    snake.drawSnake()
+   //clear the previous window
+   pen.clearRect(0,0,W,H) 
+   snake.drawSnake()
+
 }
 
 function update(){
-
+        snake.updateSnake()
 }
 
 function gameLoop() {
@@ -46,4 +62,4 @@ function gameLoop() {
 
 init();
 
-setInterval(gameLoop, 100)
+f = setInterval(gameLoop, 100)
